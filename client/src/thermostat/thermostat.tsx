821fe929aa8aa879fcda58e18e45d08eb86d2d4b
David <badcancer22@gmail.com>
2020-01-19 16:43:17 +0100

    fix interfaces

--- client/src/thermostat/thermostat.tsx
+++ client/src/thermostat/thermostat.tsx
@@ -4,25 +4,48 @@ import axios from "axios";
 import Button from "../shared/button/button";
 import styles from "./thermostat.module.scss";
 
+interface TemperatureData{
+  currentTemp: number;
+  timestamp: Date;
+  currentSetpoint: number;
+}
+
+interface LocalTemperatureData extends Omit<TemperatureData,'timestamp'>{
+  timestamp: string;
+}
+
 const Thermostat: FunctionComponent = () => {
-  const [currentData, setCurrentData] = useState({
-    temp: 0,
-    time: new Date(0).toLocaleTimeString(),
-    setTemp: 0
+  const [currentData, setCurrentData] = useState<LocalTemperatureData>({
+    currentTemp: 0,
+    timestamp: new Date(0).toLocaleTimeString(),
+    currentSetpoint: 0
   });
 
+  const fetchTemp = ()=>{
+     axios.get<TemperatureData>("http://localhost:9090").then(res => {
+      console.log(res.status);
+      if (res.status <= 200) {
+        setCurrentData({
+          currentTemp: res.data.currentTemp,
+          timestamp: new Date(res.data.timestamp).toLocaleTimeString(),
+          currentSetpoint: res.data.currentSetpoint
+        });
+      }
+    });
+  }
+
   const incrementTemp = ()=>{
-      console.log(currentData.temp,currentData.temp+.5)
+      console.log(currentData.currentTemp,currentData.currentTemp+.5)
       axios.patch("http://localhost:9090",{
-        currentSetpoint: currentData.temp+.5
+        currentSetpoint: currentData.currentTemp+.5
       }).then(res => {
         axios.get("http://localhost:9090").then(res => {
             console.log(res.status);
             if (res.status <= 200) {
               setCurrentData({
-                temp: res.data.currentTemp,
-                time: new Date(res.data.timestamp).toLocaleTimeString(),
-                setTemp: res.data.currentSetpoint
+                currentTemp: res.data.currentTemp,
+                timestamp: new Date(res.data.timestamp).toLocaleTimeString(),
+                currentSetpoint: res.data.currentSetpoint
               });
             }
           });
@@ -30,26 +53,17 @@ const Thermostat: FunctionComponent = () => {
   }
 
   useEffect(() => {
-    axios.get("http://localhost:9090").then(res => {
-      console.log(res.status);
-      if (res.status <= 200) {
-        setCurrentData({
-          temp: res.data.currentTemp,
-          time: new Date(res.data.timestamp).toLocaleTimeString(),
-          setTemp: res.data.currentSetpoint
-        });
-      }
-    });
+    fetchTemp();
   }, []);
 
   return (
     <div className={styles["thermostat-wrapper"]}>
       <div className={styles["display"]}>
         <div>
-          <div>{currentData.setTemp}</div>
-          <div>{currentData.time}</div>
+          <div>{currentData.currentSetpoint}</div>
+          <div>{currentData.timestamp}</div>
         </div>
-        <div>{currentData.temp}</div>
+        <div>{currentData.currentTemp}</div>
       </div>
       <div className={styles["button-left"]}>
         <Button
