4b116eab3783d5454bfeeaac5e4c5499d5d15435
David <badcancer22@gmail.com>
2020-01-19 14:46:10 +0100

    main setup for build

---
+++ client/src/app.tsx
@@ -0,0 +1,11 @@
+import React, {FunctionComponent} from 'react';
+
+const App: FunctionComponent = () => {
+    return(
+        <div>
+            Hello world
+        </div>
+    )
+}
+
+export default App;
